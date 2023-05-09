2050a857f5d1396b225a2ef01a63537f3fd69840
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-09 13:16:22 +0500

    Initial Commit

---
+++ FirstPipelineView/FirstPipelineViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstPipelineViewApp.swift
+//  FirstPipelineView
+//
+//  Created by sss on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstPipelineViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
